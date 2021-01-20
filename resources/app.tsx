c9e1805b89a2a81933c9f3eeb657f4348984ad7f
Wilson <wilsonhuang00815@gmail.com>
2021-01-20 15:41:14 +0800

    build: setup redux store

--- resources/app.tsx
+++ resources/app.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import rootReducer from './models'
+import './app.css'
 
-const App = () => <div>bruh bruh bruh</div>
+const store = createStore(rootReducer,
+    // @ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const App = () => (
+  <Provider store={store} />
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
