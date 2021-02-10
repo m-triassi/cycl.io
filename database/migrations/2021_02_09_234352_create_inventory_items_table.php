7d75493facf50e86d5781fd08990e7b3aa721612
Massimo Triassi <kidmax14@hotmail.com>
2021-02-09 19:11:34 -0500

    feat: #52 add Invetory migration with appropriate fields

---
+++ database/migrations/2021_02_09_234352_create_inventory_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoryItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('supplier_id')->nullable();
+            $table->string('title');
+            $table->text('description');
+            $table->float('cost');
+            $table->float('sale_price');
+            $table->integer('stock')->default(0);
+            $table->string('category')->nullable();
+            $table->string('size')->nullable();
+            $table->string('color')->nullable();
+            $table->string('finish')->nullable();
+            $table->string('material')->nullable();
+            $table->string('part_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_items');
+    }
+}
