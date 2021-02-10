65e1ddaf6db7fe0b366b1ca59164ef18554fe775
Massimo Triassi <kidmax14@hotmail.com>
2021-02-10 01:32:05 -0500

    feat: #54 add BoM migration

---
+++ database/migrations/2021_02_10_010234_create_bill_of_materials_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillOfMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_of_materials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assembly_id')->references('id')->on('inventory_items');
+            $table->foreignId('material_id')->references('id')->on('inventory_items');
+            $table->timestamps();
+
+            $table->unique(['assembly_id', 'material_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill_of_materials');
+    }
+}
