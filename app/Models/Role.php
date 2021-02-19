205f142c78652d4ec1eba0b163813b905945deb3
Massimo Triassi <kidmax14@hotmail.com>
2021-02-18 21:52:41 -0500

    fix: #69 add reverse relationship on users to roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
@@ -13,7 +14,7 @@ class Role extends Model
 
     protected $fillable = ['title', 'description', 'type'];
 
-    public function users()
+    public function users() : BelongsToMany 
     {
         return $this->belongsToMany(User::class);
     }
