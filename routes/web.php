b51eecb166c4f824cdd4a6f66f912429af51fd80
Massimo Triassi <kidmax14@hotmail.com>
2021-02-19 00:05:08 -0500

    feat: #69 add user role attaching and detaching functions

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventoryItemController;
 use App\Http\Controllers\MaterialController;
+use App\Http\Controllers\UserRoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,13 +18,13 @@ use App\Http\Controllers\MaterialController;
 
 require __DIR__.'/auth.php';
 
-Route::post('/material', [MaterialController::class, "store"])
-    ->middleware(['auth']);
+Route::put('/inventory/{id}', [InventoryItemController::class, "update"])->middleware(['auth']);
 
-Route::put('/inventory/{id}', [InventoryItemController::class, "update"])
-    ->middleware(['auth']);
-
+Route::post('/material', [MaterialController::class, "store"])->middleware(['auth']);
 Route::get('/materials/{id}', [MaterialController::class, "show"])->middleware(['auth']);
+
+Route::post('/user/roles', [UserRoleController::class, 'store']);
+Route::delete('/user/roles', [UserRoleController::class, 'destroy']);
 
 Route::get('/token', function () {
     if(config("app.debug")){
