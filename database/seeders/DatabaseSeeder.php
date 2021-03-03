2503d76fef1deafee1f796b8db0e576776eac0d5
Massimo Triassi <kidmax14@hotmail.com>
2021-03-03 17:19:52 -0500

    fix: #92 adjust seeders to appropriately attach suppliers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
          User::factory(10)->create();
-         InventoryItem::factory(25)->create();
-         Supplier::factory(5)->create();
+        Supplier::factory(5)->create();
+        InventoryItem::factory(25)->create();
     }
 }
