fe016c86c357314d281e1bdcfa68ef15e0948fdd
Massimo Triassi <kidmax14@hotmail.com>
2021-03-03 17:33:03 -0500

    feat: #92 add accessors and query scope to suppliers

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -10,8 +10,27 @@ class Supplier extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['id'];
+
     public function inventory_items() : HasMany
     {
         return $this->hasMany(InventoryItem::class);
+    }
+
+    /**
+     * attribute that can be used to check if partnerships are valid
+     * @return bool
+     */
+    public function getValidPartnershipAttribute()
+    {
+        $now = now();
+        return $now->isBefore($this->partnership_end_date) && $now->isAfter($this->partnership_start_date);
+    }
+
+    public function scopeHasValidPartnership()
+    {
+        $now = now()->format('Y-m-d');
+        return $this->where('partnership_end_date', ">", $now)
+            ->where("partnership_start_date", "<", $now);
     }
 }
