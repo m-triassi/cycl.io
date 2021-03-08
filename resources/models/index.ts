0cccd70b336086165232ae98f71c0973803351ca
Pascal <pascal.demerdjian@gmail.com>
2021-03-07 22:02:06 -0500

    feat: #99 add Vender Index

--- resources/models/index.ts
+++ resources/models/index.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux'
 import router from './router'
 import InventoryItem from './inventory'
+import VendorItem from './vendor'
 
 const rootReducer = combineReducers({
     router,
     InventoryItem,
+    VendorItem,
 })
 
 export default rootReducer
