3d69ff5840397a5ede7036f973592347db355472
Massimo Triassi <kidmax14@hotmail.com>
2021-03-10 21:09:40 -0500

    test: #104 add test that covers string interpretation of attach function

--- tests/Feature/PurchaseOrderItemTest.php
+++ tests/Feature/PurchaseOrderItemTest.php
@@ -20,7 +20,7 @@ class PurchaseOrderItemTest extends TestCase
      * @test
      * @return void
      */
-    public function test_items_can_be_updated()
+    public function test_items_can_be_updated_with_array()
     {
         $user = User::first();
         $attached = $this->actingAs($user)->put('/purchase-order/orderables/1', [
@@ -28,6 +28,33 @@ class PurchaseOrderItemTest extends TestCase
         ]);
 
         $expectedItems = InventoryItem::findMany([19, 20]);
+
+        $attachedData = $attached->getOriginalContent();
+
+        $pivotData = $attachedData['data'];
+
+        $this->assertCount(2, $pivotData);
+
+        $actualItems = InventoryItem::findMany([$pivotData[0]->id, $pivotData[1]->id]);
+
+        $attached->assertStatus(200);
+        $this->assertTrue($attachedData['success']);
+        $this->assertEquals($expectedItems, $actualItems);
+    }
+
+    /**
+     *
+     * @test
+     * @return void
+     */
+    public function test_items_can_be_updated_with_string()
+    {
+        $user = User::first();
+        $attached = $this->actingAs($user)->put('/purchase-order/orderables/1', [
+            'item_ids' => "17,18"
+        ]);
+
+        $expectedItems = InventoryItem::findMany([17, 18]);
 
         $attachedData = $attached->getOriginalContent();
 
