84bbd585dff2f698be36b250a75b9d30045ca6dd
Ian Phillips <ianjhphillips@gmail.com>
2021-03-12 22:04:36 -0500

    feat: Add PurchaseOrder Event Listener and Order Status Constants.

---
+++ app/Observers/PurchaseOrderObserver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\InventoryItem;
+use App\Models\OrderItem;
+use App\Models\PurchaseOrder;
+
+class PurchaseOrderObserver
+{
+    /**
+     * Handle the PurchaseOrder "created" event.
+     *
+     * @param  \App\Models\PurchaseOrder  $purchaseOrder
+     * @return void
+     */
+    public function created(PurchaseOrder $purchaseOrder)
+    {
+        //
+    }
+
+    /**
+     * Handle the PurchaseOrder "updated" event.
+     *
+     * @param  \App\Models\PurchaseOrder  $purchaseOrder
+     * @return void
+     */
+    public function updated(PurchaseOrder $purchaseOrder)
+    {
+        //check that status is received
+        if ($purchaseOrder->status == PurchaseOrder::RECEIVED)
+        {
+            //Get the order items corresponding to the PurchaseOrder
+            $orderItems = OrderItem::where("orderable_id", $purchaseOrder->id);
+
+            //Modify the inventory item for each order item
+            foreach ($orderItems as $orderItem) {
+                $inventoryItem = InventoryItem::find($orderItem->inventory_item_id);
+                $inventoryItem->stock = $inventoryItem->stock + $orderItem->quantity;
+                $inventoryItem->save();
+            }
+        }
+    }
+
+    /**
+     * Handle the PurchaseOrder "deleted" event.
+     *
+     * @param  \App\Models\PurchaseOrder  $purchaseOrder
+     * @return void
+     */
+    public function deleted(PurchaseOrder $purchaseOrder)
+    {
+        //
+    }
+
+    /**
+     * Handle the PurchaseOrder "restored" event.
+     *
+     * @param  \App\Models\PurchaseOrder  $purchaseOrder
+     * @return void
+     */
+    public function restored(PurchaseOrder $purchaseOrder)
+    {
+        //
+    }
+
+    /**
+     * Handle the PurchaseOrder "force deleted" event.
+     *
+     * @param  \App\Models\PurchaseOrder  $purchaseOrder
+     * @return void
+     */
+    public function forceDeleted(PurchaseOrder $purchaseOrder)
+    {
+        //
+    }
+}
