fc01136eb6f1283072ff3cd0b105403b6c7bdd99
RobertoNittolo <nitrobert@yahoo.com>
2021-03-14 22:52:57 -0400

    feat: #112 Add SupplierID to filter list for index function

--- app/Http/Controllers/InventoryItemController.php
+++ app/Http/Controllers/InventoryItemController.php
@@ -35,7 +35,8 @@ class InventoryItemController extends Controller
             "sale_price",
             "material",
             "finish",
-            "labour_cost"
+            "labour_cost",
+            "supplier_id"
         ]);
 
         if ($filters) {
