f6f23532fc9288d4e341b6391117b16e724a8378
Luigi Besani <luigi.besani@gmail.com>
2021-03-16 01:59:56 -0400

    test: #122 add tests for storing and updating purchase orders

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use App\Http\Controllers\PurchaseOrderItemController;
 require __DIR__.'/auth.php';
 
 Route::post('/purchase-order', [\App\Http\Controllers\PurchaseOrderController::class, "store"])->middleware(['auth']);
-Route::put('//purchase-order/{id}', [\App\Http\Controllers\PurchaseOrderController::class, "update"])->middleware(['auth']);
+Route::put('/purchase-order/{id}', [\App\Http\Controllers\PurchaseOrderController::class, "update"])->middleware(['auth']);
 
 Route::put('/purchase-order/orderables/{id}', [PurchaseOrderItemController::class, "update"])->middleware(['auth']);
 
