e57e17bab5a720585587cf40a7dca06522b509de
Massimo Triassi <kidmax14@hotmail.com>
2021-04-06 19:17:07 -0400

    #126 add route to access list of valid materials

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AvailableMaterialController;
 use App\Http\Controllers\SaleController;
 use App\Http\Controllers\InventoryItemController;
 use App\Http\Controllers\MaterialController;
@@ -51,10 +52,14 @@ Route::post('/inventory', [InventoryItemController::class, "store"])->middleware
 Route::put('/inventory/{id}', [InventoryItemController::class, "update"])->middleware(['auth']);
 Route::delete('/inventory/{id}', [InventoryItemController::class, "destroy"])->middleware(['auth']);
 
+Route::get('/materials/available', [AvailableMaterialController::class, "index"])->middleware(['auth']);
+
 Route::get('/materials', [MaterialController::class, "index"])->middleware(['auth']);
 Route::post('/materials', [MaterialController::class, "store"])->middleware(['auth']);
 Route::get('/materials/{id}', [MaterialController::class, "show"])->middleware(['auth']);
 Route::put('/materials/{id}', [MaterialController::class, "update"])->middleware(['auth']);
+
+
 
 Route::post('/user/roles', [UserRoleController::class, 'store'])->middleware(['auth']);
 Route::delete('/user/roles', [UserRoleController::class, 'destroy'])->middleware(['auth']);
