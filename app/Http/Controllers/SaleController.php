f902e2b7c9ef1939469579b9a10b857beba29d14
Massimo Triassi <kidmax14@hotmail.com>
2021-04-06 20:36:34 -0400

    refactor: cyc-156 remove dead code from SaleController

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -36,16 +36,6 @@ class SaleController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -169,18 +159,6 @@ class SaleController extends Controller
             'data' => $sale->refresh()
         ]);
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
 
     /**
      * Return comma-seperated string containing all sale statuses
