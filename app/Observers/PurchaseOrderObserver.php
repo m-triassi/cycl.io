0bf8cfc9679963180418e2f7329e8f88b5e3897d
Massimo Triassi <kidmax14@hotmail.com>
2021-04-19 18:42:14 -0400

    fix: #184 purchase order observer not setting delivery date on status change

--- app/Observers/PurchaseOrderObserver.php
+++ app/Observers/PurchaseOrderObserver.php
@@ -5,20 +5,23 @@ namespace App\Observers;
 use App\Models\InventoryItem;
 use App\Models\OrderItem;
 use App\Models\PurchaseOrder;
+use Carbon\Carbon;
 
 class PurchaseOrderObserver
 {
     /**
-     * Handle the PurchaseOrder "updated" event.
+     * Handle the PurchaseOrder "updating" event.
      *
      * @param  \App\Models\PurchaseOrder  $purchaseOrder
      * @return void
      */
-    public function updated(PurchaseOrder $purchaseOrder)
+    public function updating(PurchaseOrder $purchaseOrder)
     {
         //check that status is received
         if ($purchaseOrder->status == PurchaseOrder::RECEIVED)
         {
+            $purchaseOrder->delivery_date = Carbon::now();
+
             //Get the order items corresponding to the PurchaseOrder
             $orderItems = OrderItem::where("orderable_id", $purchaseOrder->id)->get();
 
